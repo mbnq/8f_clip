24d4f15901e29ade62155054af6faafcd4db2c30
mbnq <mbnq00@gmail.com>
2026-08-19 14:50:48 +0200

    Fix: prevent memory leak in AboutBoxComponent destructor

--- Source/components/LogoInfo.h
+++ Source/components/LogoInfo.h
@@ -9,15 +9,25 @@ public:
         closeButton.setButtonText("CLOSE");
         closeButton.onClick = [this]() {
             if (auto* parent = getParentComponent())
+            {
                 parent->removeChildComponent(this);
+            }
             delete this;
-            };
+        };
         addAndMakeVisible(closeButton);
 
         bandcampButton.setButtonText("https://oktafonika.bandcamp.com/");
         bandcampButton.setURL(juce::URL("https://oktafonika.bandcamp.com/"));
         bandcampButton.setColour(juce::HyperlinkButton::textColourId, juce::Colours::dodgerblue);
         addAndMakeVisible(bandcampButton);
+    }
+
+    ~AboutBoxComponent() override
+    {
+        if (auto* parent = getParentComponent())
+        {
+            parent->removeChildComponent(this);
+        }
     }
 
     void paint(juce::Graphics& g) override
